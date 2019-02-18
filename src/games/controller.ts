9a6c7a92d110a938302cb7b5205482fb384ae9f8
Izabela <izabela.nowosada@gmail.com>
2019-02-18 14:10:11 +0100

    Adjust the project

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -30,7 +30,7 @@ export default class GameController {
     await Player.create({
       game: entity, 
       user,
-      symbol: 'x'
+      turn: 'drawing'
     }).save()
 
     const game = await Game.findOneById(entity.id)
@@ -60,7 +60,7 @@ export default class GameController {
     const player = await Player.create({
       game, 
       user,
-      symbol: 'o'
+      turn: 'guessing'
     }).save()
 
     io.emit('action', {
@@ -88,8 +88,8 @@ export default class GameController {
 
     if (!player) throw new ForbiddenError(`You are not part of this game`)
     if (game.status !== 'started') throw new BadRequestError(`The game is not started yet`)
-    if (player.symbol !== game.turn) throw new BadRequestError(`It's not your turn`)
-    if (!isValidTransition(player.symbol, game.board, update.board)) {
+    if (player.turn !== game.turn) throw new BadRequestError(`It's not your turn`)
+    /* if (!isValidTransition(player.turn, game.board, update.board)) {
       throw new BadRequestError(`Invalid move`)
     }    
 
@@ -105,7 +105,7 @@ export default class GameController {
       game.turn = player.symbol === 'x' ? 'o' : 'x'
     }
     game.board = update.board
-    await game.save()
+    await game.save() */
     
     io.emit('action', {
       type: 'UPDATE_GAME',
